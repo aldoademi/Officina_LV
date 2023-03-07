c87b465d6372141b57ca4960dbb0ce34c500deb2
aldoademi <aldo.ademi011299@gmail.com>
2023-03-07 22:42:38 +0100

    Modificata un po la navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,53 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-const Nav = styled.nav`
-  background-color: #ffffff;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
-`;
-
-const NavList = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  display: flex;
-  gap: 1rem;
-`;
-
-const NavItem = styled.li`
-  font-weight: bold;
-`;
-
-const NavLink = styled(Link)`
-  text-decoration: none;
-  color: #333333;
-
-  &:hover {
-    color: #666666;
-  }
-`;
+import "../styles/navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <Nav>
-      <NavLink to="/">Officina Mobile</NavLink>
-      <NavList>
-        <NavItem>
-          <NavLink to="/servizi">Servizi</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/chisiamo">Chi siamo</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/contatti">Contatti</NavLink>
-        </NavItem>
-      </NavList>
-    </Nav>
+    <nav className="nav">
+      <Link to="/">Officina Mobile</Link>
+      <ul className={`nav-list ${isOpen ? "mobile" : ""}`}>
+        <li className={`nav-item ${isOpen ? "mobile" : ""}`}>
+          <Link to="/">Home</Link>
+        </li>
+        <li className={`nav-item ${isOpen ? "mobile" : ""}`}>
+          <Link to="/servizi">Servizi</Link>
+        </li>
+        <li className={`nav-item ${isOpen ? "mobile" : ""}`}>
+          <Link to="/chisiamo">Chi siamo</Link>
+        </li>
+        <li className={`nav-item ${isOpen ? "mobile" : ""}`}>
+          <Link to="/contatti">Contatti</Link>
+        </li>
+        <li className={`nav-button ${isOpen ? "mobile" : ""}`}>
+          <Link onClick={() => window.location.href = "tel:+393279974508"}>Chiama Ora</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
