8025cc8e8a9de2cc1b96dc864cc042b338db1e4b
Aldo <aldo.ademi011299@gmail.com>
2023-03-10 09:40:19 +0000

    Cambiato css home

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../styles/home.css"
 
 function Home() {
   return (
     <>
       <div className="home">
-        <h1>Benvenuti in Officina Mobile</h1>
+        <h1>Benvenuti in Officina Mobile LV</h1>
         <p>
         Offriamo servizi di riparazione e manutenzione di veicoli a domicilio.
         Siamo a vostra disposizione per interventi urgenti o programmabili.
